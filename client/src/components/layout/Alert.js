65411c9be8d68ea60c14331fb6897cccfc427d66
Chelsey Zhang <chelsey.zcy@gmail.com>
2022-02-22 09:05:09 +1100

    set auth action&reducer , alert component

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -16,7 +16,7 @@ Alert.propTypes = {
 };
 
 const mapStateToProps = (state) => ({
-  alerts: state.alert,
+  alerts: state.AAlert,
 });
 
 const mapDispatchToProps = {};
--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -2,7 +2,7 @@ import { SET_ALERT, REMOVE_ALERT } from "../actions/types";
 
 const initialState = [];
 
-export default function alert(state = initialState, action) {
+export default function AAlert(state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
     case SET_ALERT:
