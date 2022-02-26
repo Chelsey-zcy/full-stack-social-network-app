19fe6a77ab1181acf94813ff9753525bc178bba4
Chelsey Zhang <chelsey.zcy@gmail.com>
2022-02-26 20:37:19 +1100

    get profile reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import AAlert from "./alert";
 import auth from "./auth";
+import profile from "./profile";
 
 export default combineReducers({
   AAlert,
   auth,
+  profile,
 });
