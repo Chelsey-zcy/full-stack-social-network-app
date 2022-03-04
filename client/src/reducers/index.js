823e4dceed3b32a25c6ff1403c8a40c985780144
Chelsey Zhang <chelsey.zcy@gmail.com>
2022-03-04 17:19:30 +1100

    add post reducer to root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import AAlert from "./alert";
 import auth from "./auth";
 import profile from "./profile";
+import post from "./post";
 
 export default combineReducers({
   AAlert,
   auth,
   profile,
+  post,
 });
