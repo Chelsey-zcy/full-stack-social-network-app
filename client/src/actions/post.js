3632e3f49f792d99e017eae5abe14fd1535abc91
Chelsey Zhang <chelsey.zcy@gmail.com>
2022-03-07 18:12:43 +1100

    addPost action & reducer

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from "./types";
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST,
+} from "./types";
 
 // get all posts
 export const getPosts = () => async (dispatch) => {
@@ -69,6 +75,32 @@ export const deletePost = (postId) => async (dispatch) => {
       payload: postId,
     });
     dispatch(setAlert("The post has been Deleted", "success"));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status,
+      },
+    });
+  }
+};
+
+//create post
+
+export const addPost = (formData) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const res = await axios.post(`/api/posts`, formData, config);
+    dispatch({
+      type: ADD_POST,
+      payload: res.data,
+    });
+    dispatch(setAlert("Post created", "success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
