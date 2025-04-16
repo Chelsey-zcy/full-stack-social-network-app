16854c0928c66680d3e083da2a0379d012f11483
Chelsey Zhang <chelsey.zcy@gmail.com>
2025-04-16 15:28:52 +1000

    update dependencies

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 const auth = require("../../middleware/auth");
 const config = require("config");
 const jwt = require("jsonwebtoken");
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
 
